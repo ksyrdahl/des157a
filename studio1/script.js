e67f35c795e1bc7bdece8fe9049c9e22979b63eb
Kaelyn Syrdahl <kasyrdahl@ucdavis.edu>
2023-02-12 21:09:10 -0800

    madlibs work

--- studio1/script.js
+++ studio1/script.js
@@ -3,14 +3,10 @@
     'use strict';
     console.log('reading js');
 
-    document.querySelector('.open').addEventListener('click', function (event){
-        event.preventDefault();
-        document.getElementById('overlay').className = 'showing';
-    });
-
     document.querySelector('.close').addEventListener('click', function (event){
         event.preventDefault();
         document.getElementById('overlay').className = 'hidden';
+        document.getElementById('myform').reset();
     });
 
     document.addEventListener('keydown', function (event){
@@ -24,8 +20,9 @@
 
     const freshTips = document.querySelector('#freshTips');
 
-    myForm.addEventListener('submit', function(event){
-        event.preventDefault();
+    myForm.addEventListener('submit',function(event){
+        event.preventDefault(); 
+        document.getElementById('overlay').className = 'showing';
         const vehicle = document.querySelector('#vehicle').value;
         const adj1 = document.querySelector('#adj1').value;
         const verb1 = document.querySelector('#verb1').value;
@@ -33,49 +30,51 @@
         const verbing2 = document.querySelector('#verbing2').value;
         const adj2 = document.querySelector('#adj2').value;
         const verb2 = document.querySelector('#verb2').value;
-        let myText;
 
+        const myText =`In UC Davis, you don't need a <strong>${vehicle}</strong> to get around campus you just need a <strong>${adj1}</strong> bike. After class you can go to the MU or the Silo to <strong>${verb1}</strong>. The arboretum is a good place to relax by watching the ducks <strong>${verbing1}</strong> & the squirrels <strong>${verbing2}</strong>. But beware of the <strong>${adj2}</strong> turkeys they might <strong>${verb2}</strong> you out of the arboretum.`;
 
-        if(vehicle == ''){
-            myText ="Please provide a vehicle"
-            document.querySelector('#vehicle').focus();
-        }
-        else if(adj1 ==''){
-            myText ="Please provide an adjective"
-            document.querySelector('#adj1').focus(); 
-        }
-        else if(verb1 ==''){
-            myText ="Please provide a verb"
-            document.querySelector('#verb1').focus(); 
-        }
-        else if(verbing1 ==''){
-            myText ="Please provide a verb ending in ing"
-            document.querySelector('#verbing1').focus(); 
-        }
-        else if(verbing2 ==''){
-            myText ="Please provide a verb ending in ing"
-            document.querySelector('#verbing2').focus(); 
-        }
-        else if(adj2 ==''){
-            myText ="Please provide an adjective"
-            document.querySelector('#adj2').focus(); 
-        }
-        else if(verb2 ==''){
-            myText ="Please provide a verb"
-            document.querySelector('#verb2').focus(); 
-        }
-        else{
-            myText =`In UC Davis, you don't need a ${vehicle} to get around campus you just need a ${adj1} bike. After class you can go to the MU or the Silo to ${verb1}. The arboretum is a good place to relax by watching the ducks ${verbing1} & the squirrels ${verbing2}. But beware of the ${adj2} turkeys they might ${verb2} you out of the arboretum.`; 
-            document.querySelector('#vehicle').value = ''; 
-            document.querySelector('#adj1').value = ''; 
-            document.querySelector('#verb1').value = ''; 
-            document.querySelector('#verbing1').value = '';
-            document.querySelector('#verbing2').value = ''; 
-            document.querySelector('#adj2').value = ''; 
-            document.querySelector('#verb2').value = '';  
-        }
+        freshTips.innerHTML = myText;
 
-        freshTips.innerHTML = myText; 
-    })  
+        const splitText = freshTips.innerHtml.split("");
+
+        console.log(splitText);
+
+        // for(let i=0; i < splitText.length; i++){
+        //     myText.innerHTML += "<span>"+ splitText[i] + "</span>";
+        //     freshTips.innerHTML = myText;
+        // }
+
+        // let char = 0;
+        // let timer = setInterval(onTick, 50);
+
+        // function onTick(){
+        //     const span = myText.querySelectorAll('span')[char];
+        //     span.classList.add('fade');
+        //     char++
+        //     if(char === splitText.length){
+        //         complete();
+        //         return;
+        //     }
+        // }
+
+        // function complete(){
+        //     clearInterval(timer);
+        //     timer = null;
+        // }
+
+        // freshTips.innerHTML = myText;
+    }); 
+
+        document.querySelector('#trigger').addEventListener('mouseover', function(){
+            // from glenda: all the animation using the "animation" shortcut which takes: name, speed, duration and timing-function
+            document.querySelector('#wheelL').style.animation ='rotate 2s linear infinite';
+            document.querySelector('#wheelR').style.animation ='rotate 2s linear infinite';
+        })
+
+        document.querySelector('#trigger').addEventListener('mouseout', function(){
+            // from glenda: pause the animation-play-state
+            document.querySelector('#wheelL').style.animationPlayState ='paused';
+            document.querySelector('#wheelR').style.animationPlayState ='paused';
+        })
 
 })();
