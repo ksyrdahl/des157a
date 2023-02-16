e73cefbc82b89d6dade375de9902b141c6803456
Kaelyn Syrdahl <kasyrdahl@ucdavis.edu>
2023-02-15 17:21:16 -0800

    journal entry 4

--- journal/script.js
+++ journal/script.js
@@ -12,11 +12,18 @@
 
                 pageTop = window.pageYOffset;
                 
-                if (pageTop < 700){
-                    document.getElementById("entryNum3").style.opacity = "1";
-                    document.getElementById("entryNum2").style.opacity = "0";  
+                if (pageTop < 2500){
+                    document.getElementById("entryNum4").style.opacity = "1";
+                    document.getElementById("entryNum3").style.opacity = "0";  
                 }
-                else if (pageTop > 800 && pageTop < 1700 ){
+                else if (pageTop > 2500 && pageTop < 3500 ){
+                    document.getElementById("entryNum4").style.opacity = "0";
+                    document.getElementById("entryNum3").style.opacity = "1";
+                    document.getElementById("entryNum2").style.opacity = "0";
+                    document.getElementById("entryNum1").style.opacity = "0";
+                } 
+                else if (pageTop > 3500 && pageTop < 4500 ){
+                    document.getElementById("entryNum4").style.opacity = "0";
                     document.getElementById("entryNum3").style.opacity = "0";
                     document.getElementById("entryNum2").style.opacity = "1";
                     document.getElementById("entryNum1").style.opacity = "0";
