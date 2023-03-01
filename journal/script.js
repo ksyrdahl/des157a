7ec2eaab7efab4f4e1d6e97477fcd7b9e6dfa012
Kaelyn Syrdahl <kasyrdahl@ucdavis.edu>
2023-03-01 15:58:04 -0800

    journal entry 5

--- journal/script.js
+++ journal/script.js
@@ -12,25 +12,40 @@
 
                 pageTop = window.pageYOffset;
                 
-                if (pageTop < 2500){
-                    document.getElementById("entryNum4").style.opacity = "1";
-                    document.getElementById("entryNum3").style.opacity = "0";  
+                if (pageTop < 800){
+                    document.getElementById("entryNum5").style.opacity = "1";
+                    document.getElementById("entryNum4").style.opacity = "0";
+                    document.getElementById("entryNum3").style.opacity = "0";
+                    document.getElementById("entryNum2").style.opacity = "0";
+                    document.getElementById("entryNum1").style.opacity = "0";  
                 }
-                else if (pageTop > 2500 && pageTop < 3500 ){
+                else if (pageTop > 800 && pageTop < 3500 ){
+                    document.getElementById("entryNum5").style.opacity = "0";
+                    document.getElementById("entryNum4").style.opacity = "1";
+                    document.getElementById("entryNum3").style.opacity = "0";
+                    document.getElementById("entryNum2").style.opacity = "0";
+                    document.getElementById("entryNum1").style.opacity = "0";
+                }
+                else if (pageTop > 3500 && pageTop < 4500 ){
+                    document.getElementById("entryNum5").style.opacity = "0";
                     document.getElementById("entryNum4").style.opacity = "0";
                     document.getElementById("entryNum3").style.opacity = "1";
                     document.getElementById("entryNum2").style.opacity = "0";
                     document.getElementById("entryNum1").style.opacity = "0";
-                } 
-                else if (pageTop > 3500 && pageTop < 4500 ){
+                }  
+                else if (pageTop > 4500 && pageTop < 5500 ){
+                    document.getElementById("entryNum5").style.opacity = "0";
                     document.getElementById("entryNum4").style.opacity = "0";
                     document.getElementById("entryNum3").style.opacity = "0";
                     document.getElementById("entryNum2").style.opacity = "1";
                     document.getElementById("entryNum1").style.opacity = "0";
                 }    
                 else{
-                    document.getElementById("entryNum1").style.opacity = "1";
+                    document.getElementById("entryNum5").style.opacity = "0";
+                    document.getElementById("entryNum4").style.opacity = "0";
+                    document.getElementById("entryNum3").style.opacity = "0";
                     document.getElementById("entryNum2").style.opacity = "0";
+                    document.getElementById("entryNum1").style.opacity = "1";
                 }
 
             });
